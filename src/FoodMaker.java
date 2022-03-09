d1e50b1a65b6c84e24289e229c5261d4277acd0d
Saxophilyman <zd_@mail.ru>
2022-03-10 00:11:22 +0400

    Part_I "What is Pizza? 0.2" (sketch)
     - some thoughts about structure

--- src/FoodMaker.java
+++ src/FoodMaker.java
@@ -1,12 +1,23 @@
 public class FoodMaker {
     String nameOfFoodMaker;
+    //Время приготовления для всех заказов одно и то же
+    final int TIME_OF_COOKING;
 
-    private void getOrderToCookingByFoodMaker(){
-
+    {
+        TIME_OF_COOKING = 3000;
     }
 
-    private void makeEatByFoodMaker(){
+    private boolean getOrderToCookingByFoodMaker(){
+        if (/*для заказа хватает ингредиентов || заказ пролежал в очереди дольше N секунд (задаётся константой)*/){
 
+        }
+            else {/*Если ни одного заказа нет, то повар ждёт M секунд (константа) и проводит повторную проверку очереди*/}
+    }
+
+    private boolean makeEatByFoodMaker(){
+        if (getOrderToCookingByFoodMaker()){
+            /*Как только повар берёт заказ в работу, со склада списывается необходимое количество нужных ингредиентов*/
+        }
     }
 
 }
