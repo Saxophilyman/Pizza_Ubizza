5e1984e790e09dc8f410f632754b8b7e79f45955
Saxophilyman <zd_@mail.ru>
2022-03-21 22:36:59 +0400

    Part_I "What is Pizza? 0.4" (sketch)
     - some thoughts about structure

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,9 @@
 public class Pizza {
+    private SizeOfPizza sizeOfPizza;
+    private TypeOfPizza typeOfPizza;
+
+    public Pizza(SizeOfPizza sizeOfPizza, TypeOfPizza typeOfPizza) {
+        this.sizeOfPizza = sizeOfPizza;
+        this.typeOfPizza = typeOfPizza;
+    }
 }
