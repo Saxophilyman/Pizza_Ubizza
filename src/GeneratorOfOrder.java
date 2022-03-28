0a89df6754c25e36dbc3887c4fcab603cff90ead
Saxophilyman <zd_@mail.ru>
2022-03-29 01:29:02 +0400

    Part_I "What is Pizza? 0.6" (sketch)
     - some thoughts about structure
     - how make a pizza?

--- src/GeneratorOfOrder.java
+++ src/GeneratorOfOrder.java
@@ -1,11 +1,18 @@
+import java.util.ArrayList;
 import java.util.Random;
-
+//Integer,String,String
 public class GeneratorOfOrder implements Runnable {
+    ArrayList<Pizza> listInformationOfOrder = new ArrayList();
 
+    //временная переменная
+    private int countForDelete = 100;
+    //Программа должна корректно завершаться после обработки N заказов
+    //(N получать в виде параметра командной строки, 10 <= N <= 1000).
 
     private void addOrderToQueue(){
+        listInformationOfOrder.add((new Pizza(getRandomSizeOfPizza(),getRandomTypeOfPizza()));
     }
-
+//добавить порядковый номер, возможно через Map
     private  TypeOfPizza getRandomTypeOfPizza(){
         Random random = new Random();
         return TypeOfPizza.values()[random.nextInt(TypeOfPizza.values().length)];
@@ -18,7 +25,12 @@ public class GeneratorOfOrder implements Runnable {
 
     @Override
     public void run() {
-
+        int countForStopGeneratorOfOrder = 0; //или порядковый номер заказа
+        while (countForStopGeneratorOfOrder<countForDelete){
+        //Случайное время
+        addOrderToQueue();
+        countForStopGeneratorOfOrder++;
+        }
     }
 }
     /*При совершении заказа выводить:
